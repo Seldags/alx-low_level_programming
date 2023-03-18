92942d377d851f2ca984654413d4872d73fab05f
Seldags <fosterdags@hotmail.com>
2023-03-17 17:18:42 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
- * Return:1(success)
+ * Return:1
  */
-int main(void)
+iint main(void)
 {
-	fputs(" and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",stdout);
-	return (1);
+fput("and that piece of art is useful\" - Dora Korpar , 2015-10-19\n" , stdout);
+return (1);
 }
