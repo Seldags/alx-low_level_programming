69cf030aa7562fc1391e595a7e4b16bc880f7883
Seldags <fosterdags@hotmail.com>
2023-03-22 05:47:13 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
-#1fndef main_h
+#ifndef main_h
 #define main_h
+int _putchar(char);
 void print_alphabet(void); 
 void print_alphabet_x10(void);  
 int _islower(int c);  
@@ -7,8 +8,10 @@ int _isalpha(int c);
 int print_sign(int n);  
 int _abs(int); 
 int print_last_digit(int);  
-void jack_bauer(void);  
+void jack_bauer(void);
+int add(int,int);
 void times_table(void);
 void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
