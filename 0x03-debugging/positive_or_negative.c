977102218745a4008b5bf1d91a6b5d6cb8f0bdac
Seldags <fosterdags@hotmail.com>
2023-03-24 15:52:52 -0700

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -5,7 +5,7 @@ void positive_or_negative(int i)
 if (i > 0)
 printf("%d ispositive\n", i);
 else if (i < 0)
-printf("%d is nagative\n" , i);
+printf("%d is nagative\n", i);
 else
 printf("%d is zero\n", i);
 }
